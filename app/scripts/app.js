73680d3ee0cf03c370be751862f1ba4f634d5c85
Yuri <jekonik26@gmail.com>
2017-10-01 15:52:15 +0300

    add_media-playerjs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,18 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'mediaelement';
 
 $(() => {
 	svg4everybody();
 });
+
+
+$('#youtubevideo').mediaelementplayer({
+    videoWidth: '100%',
+    videoHeight: '100%',
+    plugins: ['flash'],
+    pluginPath: '/swf/',
+    flashName: 'flashmediaelement.swf'
+});
+
+
