25f73f6b012e1084da344cc1f0d25ac9fe0d8572
Yuri <jekonik26@gmail.com>
2017-10-04 16:13:18 +0300

    gulp-settigns/update/change-tasks

---
+++ tasks/stylus-font.js
@@ -0,0 +1,15 @@
+// var gulp = require('gulp');
+// var gulpStylus = require('gulp-stylus');
+// var fontAwesome = require('font-awesome-stylus');
+//
+// gulp.task('build', function () {
+//     return gulp.src('app/styles/*.styl')
+//         .pipe(gulpStylus({
+//             use: [
+//                 fontAwesome(),
+//             ]
+//         }))
+//         .pipe(gulp.dest('dist/assets/fonts'));
+// });
+//
+//
