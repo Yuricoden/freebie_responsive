43faa0538f755a5aa0e47aaf26d52da65d520777
Yuri <jekonik26@gmail.com>
2017-10-07 19:28:56 +0300

    add_js_code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import 'mediaelement';
-
+import 'fullpage.js'
 $(() => {
 	svg4everybody();
 });
@@ -20,7 +20,7 @@ var btnTop = $('.footer__arrow-up');
 
 $(document).ready(function() {
     btnBottom.on('click', (function() {
-    $('html,body').animate({ scrollTop: $(document).height() }, 'slow');
+    $('html,body').animate({ scrollTop: $(document).height() }, 1000);
     return false
     }));
 
@@ -41,6 +41,53 @@ $(".items__item-like").on('animationend', function(){
     $(this).toggleClass('animating');
 });
 
+/* simple hard codex */
+var text_service = document.getElementsByClassName('service-desctiption__read-description')[0];
+var button_service = document.getElementsByClassName('service-desctiption__read-button')[0];
+var text_about = document.getElementsByClassName('about-description__read-description')[0];
+var button_about = document.getElementsByClassName('about-description__read-button')[0];
+function  readmore() {
+            button_service.onclick = function () {
+                if (text_service.clientHeight < 155) {
+                    text_service.style.height = 155 + 'px'
+                } else {
+                    text_service.style.height = 50 + 'px'
+                }
+            }
+
+            button_about.onclick = function () {
+                 if (text_about.clientHeight < 155) {
+                         text_about.style.height = 155 + 'px'
+                 } else {
+                        text_about.style.height = 50 + 'px'
+                 }
+            }
+ }
+
+readmore()
 
 
 
+var blogContainer = document.getElementsByClassName('blog-container__blog-items')[0].clientHeight;
+var buttonShow = document.getElementsByClassName('blog-container__button-show')[0];
+var child = document.getElementsByClassName('blog-container__blog-items')[0].childNodes;
+console.log(child)
+function blogItems(container, show) {
+    if (blogContainer > (container / 2) )  {
+     show.onclick = function onHide() {
+            document.getElementsByClassName('blog-container__blog-items')[0].classList.add('overflow_blog-item');
+            return  show.onclick = function onShow() {
+                document.getElementsByClassName('blog-container__blog-items')[0].classList.remove('overflow_blog-item');
+                return blogItems(container,show)
+            }
+          }
+        }
+    }
+blogItems(blogContainer,buttonShow)
+
+
+$('a[href^="#"]').click(function(){
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+    return false;
+});
